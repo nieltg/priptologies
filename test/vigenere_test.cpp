51465df8b65d9f8ba0517d449288f58a7cebfac5
Daniel <nieltansah@gmail.com>
2019-01-29 12:29:19 +0700

    vigenere: Fix test messages, add test for empty cipher-text decryption

--- test/vigenere_test.cpp
+++ test/vigenere_test.cpp
@@ -11,14 +11,18 @@ protected:
   Vigenere<char> cipher_ = Vigenere<char>("key");
 };
 
-TEST_F(VigenereTest, empty_plain_text_works) {
+TEST_F(VigenereTest, encrypt_empty_plain_text_works) {
   ASSERT_TRUE(cipher_.encrypt("") == "");
 }
 
-TEST_F(VigenereTest, key_lengthed_plain_text_works) {
+TEST_F(VigenereTest, encrypt_key_lengthed_plain_text_works) {
   ASSERT_TRUE(cipher_.encrypt("abc") == "\xcc\xc7\xdc");
 }
 
-TEST_F(VigenereTest, plain_text_with_length_more_than_key_length_works) {
+TEST_F(VigenereTest, encrypt_plain_text_with_length_more_than_key_length_works) {
   ASSERT_TRUE(cipher_.encrypt("abcabc") == "\xcc\xc7\xdc\xcc\xc7\xdc");
+}
+
+TEST_F(VigenereTest, decrypt_empty_plain_text_works) {
+  ASSERT_TRUE(cipher_.decrypt("") == "");
 }
